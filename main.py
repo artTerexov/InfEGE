14b0c8b38c0d369cd03bd6ce4c75136751c99dc7
artHouse <art133140@gmail.com>
2021-05-16 11:23:32 +0300

    Commit 10

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-# print(bin(77))
-# print(int("10011", 2))
-print(6**5 - 5**5)
+for i in range(1, 794):
+    if i % 4 == 0 and i % 8 != 0:
+        print(i)
+
+print(bin(788))
+print(int("1100011001", 2))
+print(bin(780))

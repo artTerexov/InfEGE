288977bc3bc4d46ce370d0ec8832cf9e5449ed53
artHouse <art133140@gmail.com>
2021-05-18 17:59:52 +0300

    Commit 13

--- Fedor/task14_1.py
+++ Fedor/task14_1.py
@@ -1,14 +1,14 @@
-# num = 5 * 216 ** 1156 - 4 * 36 ** 1147 + 6 ** 1153 - 875
-# base = 6
-#
-# buff = ""
-# while num // base > 0:
-#     buff += str(num % base)
-#     num = num // base
-# buff += str(num)
-#
-# buff = buff[::-1]
-#
-# print(buff.count("5") - buff.count("0"))
+num = 5 * 216 ** 1156 - 4 * 36 ** 1147 + 6 ** 1153 - 875
+base = 6
+
+buff = ""
+while num // base > 0:
+    buff += str(num % base)
+    num = num // base
+buff += str(num)
+
+buff = buff[::-1]
+
+print(buff.count("5") - buff.count("0"))
 
 print(9 * 5 ** 4)

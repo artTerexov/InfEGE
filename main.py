785ff87e1ab11204cd15f1afcb18be79bd637859
artHouse <art133140@gmail.com>
2021-05-27 11:56:59 +0300

    Commit 16

--- main.py
+++ main.py
@@ -42,21 +42,48 @@
 #     if deliteli(j):
 #         continue
 
-with open('27-18b.txt') as f:
-    s = f.read().strip().split('\n')
+# with open('27-18b.txt') as f:
+#     s = f.read().strip().split('\n')
+#
+#
+# size = s[0]
+# s.pop(0)
+# generator = [int(i) for i in s]
+# count = 0
+# for j in range(0, len(generator) - 1):
+#     for h in range(j + 1, j + 5):
+#         if h > len(generator) - 1:
+#             break
+#         proiz = generator[j] * generator[h]
+#         summa = generator[j] + generator[h]
+#         if proiz % 13 == 0 and summa % 2 != 0:
+#             count += 1
+#
+# print(count)
 
 
-size = s[0]
-s.pop(0)
-generator = [int(i) for i in s]
-count = 0
-for j in range(0, len(generator) - 1):
-    for h in range(j + 1, j + 5):
-        if h > len(generator) - 1:
-            break
-        proiz = generator[j] * generator[h]
-        summa = generator[j] + generator[h]
-        if proiz % 13 == 0 and summa % 2 != 0:
-            count += 1
+import string
+import time
 
-print(count)
+def is_pangram(s):
+    template = {i for i in range(65, 91)}
+    buff = set()
+    s = s.upper()
+    for i in s:
+        if 65 <= ord(i) <= 90:
+            buff.add(ord(i))
+    if template == buff:
+        return 1
+    else:
+        return 0
+
+# def is_pangram(s):
+#     return set(string.ascii_lowercase) <= set(s.lower())
+
+start_time = time.time()
+
+pangram = "The quick, brown fox jumps over the lazy dog! The quick, brown fox jumps over the lazy dog! The quick, brown fox jumps over the lazy dog! The quick, brown fox jumps over the lazy dog! The quick, brown fox jumps over the lazy dog! The quick, brown fox jumps over the lazy dog! The quick, brown fox jumps over the lazy dog!"
+
+print(is_pangram(pangram))
+print("--- %s seconds ---" % (time.time() - start_time))
+all

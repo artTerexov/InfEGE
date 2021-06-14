a53d70cb2f8b8833f0bae5fc17d0edf6c480c6e5
artHouse <art133140@gmail.com>
2021-06-14 13:27:30 +0300

    commit24

--- Danila/task___.py
+++ Danila/task___.py
@@ -1,2 +1,13 @@
-print(bin(161))
-print(int("10101", 2))
+for i in range(1, 1000):
+    x = i
+    L = 0
+    M = 0
+    while x > 0:
+        M = M + 1
+        if x % 2 != 0:
+            L = L + 1
+        x = x // 2
+    # print(L)
+    # print(M)
+    if L == 5 and M == 8:
+        print(i)

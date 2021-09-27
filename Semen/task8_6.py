77622338d4d4802ca160a4bd38675caf17baabc8
artHouse <art133140@gmail.com>
2021-09-27 19:51:54 +0300

    update

--- Semen/task8_6.py
+++ Semen/task8_6.py
@@ -1,13 +1,3 @@
 # Мила составляет 4-значные числа в 8-ичной системе. Сколько различных чисел, делящихся на 4
 # без остатка, может составить Мила?
 
-import itertools
-
-s = "01234567"
-a = set(itertools.permutations(s, r=4))
-count = 0
-
-for i in a:
-    b = "".join(i)
-
-print(count)

547db728686b5fe18c2dbe7d644b25c71a41e921
artHouse <art133140@gmail.com>
2022-11-05 16:24:17 +0300

    update

---
+++ Michael/task9/5521.py
@@ -0,0 +1,25 @@
+# чтение файла
+# /t - табуляция /n - перенос на строку
+
+def countDouble(a: list) -> bool:
+    return True
+
+
+def average(a: list) -> bool:
+    return True
+
+
+with open('5521') as f:
+    s = f.readlines()
+
+
+# for i in s:
+#     if countDouble(i.split()) and average(i.split()):
+#         print('OK')
+#     break
+
+
+F = False
+
+if not F:
+    print('OK')

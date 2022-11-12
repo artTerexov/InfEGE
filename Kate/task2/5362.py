7594708022d77b4c4a6a9cd9c9aeb7c0e943df00
artHouse <art133140@gmail.com>
2022-11-12 20:31:54 +0300

    update

---
+++ Kate/task2/5362.py
@@ -0,0 +1,10 @@
+# ((x → z) → y) ∨ ¬w
+
+print('x y z w F')
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                F = ((x <= z) <= y) or not w
+                if F == 0:
+                    print(x, y, z, w, F)

60d46969e24ffa5b069b55be5a10a99ad2fde574
ATerekhov <avterehov@msk.vtb.ru>
2023-01-16 18:58:20 +0300

    add new programm

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import time
-
-start_time = time.time()
-
-a = 1000000000
-c = 0
-
-for i in range(a):
-    c += 1
-
-print(c)
-
-print("--- %s seconds ---" % (time.time() - start_time))
+print(1980 / 60)

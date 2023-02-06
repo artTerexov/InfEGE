b931eb00843e355de3edf7d341fd8b879dcfbccf
ATerekhov <avterehov@msk.vtb.ru>
2023-02-06 21:39:37 +0300

    add new programm

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-from turtle import *
-tracer(0, 0)
-goto(0, 0)
-left(90)
-for I in range(10):
-    goto(xcor() + 200, ycor() + 100)
-    goto(xcor() - 50, ycor() - 150)
-    goto(xcor() - 150, ycor() + 50)
-penup()
-# for x in range(-10, 40):
-#     for y in range(-30, 10):
-#         goto(x, y)
-#         dot(10)
-update()
-done()
+print(478 * 131072 / 1024)

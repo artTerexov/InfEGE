6a4acc5591400290d192657a2f3032991b1db154
ATerekhov <avterehov@msk.vtb.ru>
2023-03-15 20:22:37 +0300

    add new programm

---
+++ Artem/task26/26-6759.py
@@ -0,0 +1,14 @@
+# with open('files/6759') as f:
+#     a = f.read()
+# a = a.split('\n')
+# a.pop(0)
+# a = list(map(int, a))
+# c = len(a) // 3
+# a.sort(reverse=True)
+# s = sum(a)
+# for i in range(0, c):
+#     s -= a[i]
+# print(s)
+
+
+

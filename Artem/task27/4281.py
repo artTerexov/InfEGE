c2b2ad2773e30bcda85b3aaa37d2e7aa193ca8f8
artHouse <art133140@gmail.com>
2023-03-19 17:14:27 +0300

    update

--- Artem/task27/4281.py
+++ Artem/task27/4281.py
@@ -1,21 +1,27 @@
-with open('files/27-75b.txt') as f:
-    a = f.read().strip().split('\n')
-a.pop(0)
-a = list(map(int, a))
-D = 43
-c = 0
-maxSum = 0
+with open('files/4281_B.txt') as f:
+    s = [int(i) for i in f.readlines()]
+
+s.pop(0)
+d = 43
+a1 = [0] * d
+a2 = [0] * d
+a2[0] = -1
+summ = 0
+maxSumma = 0
 maxLen = 0
-for i in range(len(a)):
-    x = i
-    k = 0
-    while x != (len(a)):
-        k = a[x] + k
-        x += 1
-        if k % D == 0 and maxSum <= k:
-            if maxSum == k:
-                maxLen = min(maxLen, x - i)
-            else:
-                maxLen = x - i
-            maxSum = k
+
+for i in range(len(s)):
+    summ += s[i]
+    ostat = summ % d
+    if a1[ostat] == 0 and ostat != 0:
+        a1[ostat] = summ
+        a2[ostat] = i
+    elif maxSumma < summ - a1[ostat]:
+        maxSumma = summ - a1[ostat]
+        maxLen = i - a2[ostat]
+    elif maxSumma == summ - a1[ostat]:
+        maxLen = min(maxLen, i - a2[ostat])
+
+
 print(maxLen)
+
